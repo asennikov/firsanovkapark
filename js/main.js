c2c982fe98ab1e7e885ebd5b0bfe90dbb96343de
Alexander Sennikov <gseno88@gmail.com>
2015-04-10 13:37:21 +0300

    Add map and slides for all frames

--- js/main.js
+++ js/main.js
@@ -18,4 +18,86 @@ $(document).ready(function(){
     paddingTop: 95
   });
 
+  var options = {
+      zoom: 14,
+      center:  new google.maps.LatLng(55.967967, 37.280199),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+  };
+
+  map = new google.maps.Map($('.cover-frame-map-container')[0], options);
+  map.setOptions({
+    styles: [
+      {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+          { "color": "#66814D" }
+        ]
+      },{
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+          { "color": "#566447" }
+        ]
+      },{
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+          { "color": "#000000" }
+        ]
+      },{
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+          { "color": "#3E3A27" }
+        ]
+      },{
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+          { "color": "#ffffff" },
+          { "visibility": "simplified" },
+          { "lightness": -22 }
+        ]
+      },{
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          { "color": "#ffffff" },
+          { "visibility": "on" },
+          { "lightness": -22 }
+        ]
+      },{
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          { "color": "#000000" },
+          { "lightness": 23 }
+        ]
+      },{
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          { "color": "#ffffff" },
+          { "lightness": -22 }
+        ]
+      },{
+        "featureType": "poi",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          { "color": "#000000" },
+          { "lightness": 23 }
+        ]
+      },{
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          { "color": "#000000" },
+          { "lightness": 23 }
+        ]
+      }
+    ]
+  });
+
 });
