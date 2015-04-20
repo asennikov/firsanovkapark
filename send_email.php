3f2820a5bc4d9100c34dccd92302f3e61d0a1aef
Alexander Sennikov <gseno88@gmail.com>
2015-04-20 18:31:18 +0300

    Return result in json

--- send_email.php
+++ send_email.php
@@ -21,13 +21,11 @@
         )
       );
       $result = $mandrill->messages->send($message);
-      print_r($result);
-
+      echo(json_encode($result));
+      
     } catch(Mandrill_Error $e) {
       // Mandrill errors are thrown as exceptions
       echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
-      // A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-      throw $e;
     }
 
     die();
